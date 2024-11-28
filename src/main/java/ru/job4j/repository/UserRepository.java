66dbe629f86c0d1e71336807698bf83317e166a0
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2024-11-28 18:31:55 +0300

    Spring Data JPA [#505298]

--- src/main/java/ru/job4j/repository/UserRepository.java
+++ src/main/java/ru/job4j/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package ru.job4j.repository;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.job4j.model.User;
 
 import java.util.List;
 
-public interface UserRepository {
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findAll();
 
     User findByClientId(Long clientId);
 
-    void save(User user);
+    User save(User user);
 }
